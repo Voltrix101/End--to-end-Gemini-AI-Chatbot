4a64061ef36ac353e7ad150ae86b1dd981d4727b
Amrito Ballav Roy <amritob0327.roy@gmail.com>
2024-04-14 23:55:12 +0530

    Update qachat.py
    bug fixes

--- qachat.py
+++ qachat.py
@@ -5,7 +5,7 @@ import streamlit as st
 import os
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAOvFs11qB0-cYjjoNmZ5qAqhuFb6lGTPU")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## function to load Gemini Pro model and get repsonses
 model=genai.GenerativeModel("gemini-pro") 
